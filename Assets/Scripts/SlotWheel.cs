4c2fac5c0bd9bd7a6dc10111aaf2173c9da5b478
Darya Tikhanovich <daryatikhanovich@gmail.com>
2023-08-08 01:04:41 +0300

    did well stopping

--- Assets/Scripts/SlotWheel.cs
+++ Assets/Scripts/SlotWheel.cs
@@ -16,10 +16,11 @@ namespace DefaultNamespace
         
         private float _speed;
         private const float BaseSpeed = 4f;
-        
+        private float _distanceCell;
+
         private static Vector3 startPos = new Vector3(0f, 400f, 0f);
         private static Vector3 endPos = new Vector3(0f, -400f, 0f);
-        private float symbolHeight;
+        private float symbolHeight = 200;
         
         public bool isMove;
         private Coroutine _coroutine;
@@ -29,10 +30,6 @@ namespace DefaultNamespace
         private void Awake()
        {
            _symbols = GetComponentsInChildren<Symbol>().ToList();
-           if (_symbols.Count > 0)
-           {
-               symbolHeight = _symbols[0].transform.localScale.y; 
-           }
        }
         
        private void Start()
@@ -54,15 +51,38 @@ namespace DefaultNamespace
        {
            if (isMove)
            {
-               for (int i = 0; i < _symbols.Count; i++)
+               if (wheelStates == WheelsStates.stopping)
                {
-                   _symbols[i].transform.Translate(Vector3.down * _speed, Space.Self);
-                   if (_symbols[i].transform.localPosition.y <= endPos.y)
+                   var distLocal = _distanceCell + 400;
+                   foreach (Symbol symbol in _symbols)
                    {
-                       var pos = _symbols[i].transform.localPosition;
-                       pos.y = startPos.y;
-                       SetRandom(_symbols[i]);
-                       _symbols[i].transform.localPosition = pos;
+                       var y = distLocal;
+                       while (y <= endPos.y)
+                       {
+                           y += (symbolHeight * 4);
+                       }
+
+                       var d = new Vector3(
+                           symbol.transform.localPosition.x, 
+                           y, 
+                           symbol.transform.localPosition.z);
+                       symbol.transform.localPosition = d;
+                       
+                       distLocal -= symbolHeight;
+                   }
+               }
+               else
+               {
+                   for (int i = 0; i < _symbols.Count; i++)
+                   {
+                       _symbols[i].transform.Translate(Vector3.down * _speed, Space.Self);
+                       if (_symbols[i].transform.localPosition.y <= endPos.y)
+                       {
+                           var pos = _symbols[i].transform.localPosition;
+                           pos.y = pos.y + 800;
+                           SetRandom(_symbols[i]);
+                           _symbols[i].transform.localPosition = pos;
+                       }
                    }
                }
            }
@@ -72,14 +92,10 @@ namespace DefaultNamespace
        public void StartMove()
        {
             wheelStates = WheelsStates.moving;
-            
-            float distanceToStop = symbolHeight * 10; 
-            float timeToStop = distanceToStop / BaseSpeed;
-           
-           DOTween.To(() => _speed, x => _speed = x, BaseSpeed, 0.5f).OnStart((() =>
+            DOTween.To(() => _speed, x => _speed = x, BaseSpeed, 0.5f).OnStart((() =>
            {
                isMove = true;
-               _coroutine = StartCoroutine(StopTimer(timeToStop));
+               _coroutine = StartCoroutine(StopTimer());
            }));
        }
        
@@ -92,8 +108,16 @@ namespace DefaultNamespace
                StopCoroutine(_coroutine);
                _coroutine = null;
            }
-           DOTween.To(() => _speed, x => _speed = x, 0, 0.5f).OnComplete(() =>
+
+           float start = _symbols[0].transform.localPosition.y - 400; //позиция символа на старте остановки 
+           float end = start - symbolHeight * 3; // позиция символа в которой он должен остановиться (неровная позиция)
+           float endAdjusted = (float)Math.Round(end / symbolHeight) * symbolHeight; // выровненная позиция 
+           _distanceCell = start; //ячейка для dotweena для хранения текущей позиции самого верхнего символа 
+           
+           DOTween.To(() => _distanceCell, x => _distanceCell = x, endAdjusted, 0.5f).OnComplete(() =>
            {
+               Debug.Log($"{start} {end} {endAdjusted} {symbolHeight}");
+               
                isMove = false;
                wheelStates = WheelsStates.notMoving;
                
@@ -111,12 +135,12 @@ namespace DefaultNamespace
            //set reward
        }
        
-       private IEnumerator StopTimer(float TimeToStop)
+       private IEnumerator StopTimer()
        {
-           yield return new WaitForSeconds(TimeToStop);
+           yield return new WaitForSeconds(5.0f);
            _coroutine = null;
            OnStopSpinning?.Invoke();
-          StopMove();
+          
        }
        
        private void SetRandom(Symbol symbol)
