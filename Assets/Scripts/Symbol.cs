c449eb5ed0c986be48d871681ee2f5983abf1ce2
Darya Tikhanovich <daryatikhanovich@gmail.com>
2023-09-12 12:02:52 +0300

    add symbolHeight

--- Assets/Scripts/Symbol.cs
+++ Assets/Scripts/Symbol.cs
@@ -14,6 +14,8 @@ namespace DefaultNamespace
         private SymbolModel _model;
 
         [SerializeField] public int symbolId;
+
+        private readonly float _symbolHeight = 200f;
         
         private void Awake()
         {
@@ -28,22 +30,20 @@ namespace DefaultNamespace
             _model.ShowRandomImage += UpdateImage;
             _model.ShowFinalImage += UpdateFinalImage;
             UpdateImage();
-            //метод initialize для инициализации принимает два параметра и сохраняет их в соответствующие приватные поля
-            //еще метод подписывается на определенные события из класса SymbolModel
         }
 
         private void UpdatePosition(double position)
         {
-            float y = 400f - 200f * (float)position;
+            float y = _symbolHeight * 2 - _symbolHeight * (float)position;
             transform.localPosition = new Vector3(0, y, 0);
         }
 
-        private void UpdateImage() //обновляет изображение символа, на случайный
+        private void UpdateImage()
         {
             _image.sprite = _spriteProvider.GetNextRandomSprite(); 
         }
 
-        private void UpdateFinalImage(int finalIndex) //устанавлиает окончельное изображение на основе переданного finalIndex
+        private void UpdateFinalImage(int finalIndex)
         {
             _image.sprite = _spriteProvider.GetFinalSprite(finalIndex);
         }
